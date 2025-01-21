0c4136e7b23285517d154b483880a5918dc97d02
DVD314 <gordondmitrii@gmail.com>
2025-01-21 16:27:23 +0300

    Bot sends images to user

--- bot.py
+++ bot.py
@@ -3,12 +3,12 @@ import telebot
 import prompt_handler
 from random import randint
 from json import load
+import time
+import os
 
 token = load(open("config.json", "r"))["token"]
 
 bot = telebot.TeleBot(token)
-
-sent_files = set()
 
 WATCH_FOLDER = load(open("config.json", "r"))["output_folder"]
 
@@ -17,37 +17,35 @@ def txt2img(message: telebot.telebot.types.Message):
     content = message.text
 
     prompt = prompt_handler.txt2prompt(content)
+    prompt["parameters"]["chat_id"] = message.chat.id
 
     api.send_prompt(model_name=prompt["model"], values=prompt["parameters"])
 
 
-def send_file_to_user(file_name, file_path):
+def send_file_to_chat(file_name, file_path):
     try:
-        user_id = file_name.split('_')[0]
-        if not user_id.isdigit():
+        chat_id = file_name.split('_')[0]
+        if not chat_id.isdigit():
             return f"Некорректное имя файла: {file_name}"
-        user_id = int(user_id)
+        chat_id = int(chat_id)
         with open(file_path, 'rb') as img:
-            bot.send_photo(user_id, img, caption=str(randint(0, 999999)) + ".png")
-        return f"Файл {file_name} отправлен пользователю {user_id}."
+            bot.send_photo(chat_id, img)
+        return f"Файл {file_name} отправлен пользователю {chat_id}."
     except Exception as e:
         return f"Ошибка отправки файла {file_name}: {e}"
 
 def check_new_files():
-    global sent_files
     files = os.listdir(WATCH_FOLDER)
     for file_name in files:
         # Проверяем, является ли файл изображением
-        if file_name.endswith(('.jpg', '.png', '.jpeg')):
+        if not file_name.startswith("(sent)") and file_name.endswith(('.jpg', '.png', '.jpeg')):
             file_path = os.path.join(WATCH_FOLDER, file_name)
-            # Проверяем, отправлялся ли этот файл ранее
-            if file_name not in sent_files:
-                # Отмечаем файл как отправленный
-                sent_files.add(file_name)
-                # Отправляем файл пользователю
-                send_file_to_user(file_name, file_path)
+            # Отправляем файл пользователю
+            print(send_file_to_chat(file_name, file_path))
+            os.rename(WATCH_FOLDER + "\\" + file_name, "sent\\" + str(randint(0, 10 ** 16)) + file_name)
+            
 
 def monitor_folder():
     while True:
         check_new_files()
-        time.sleep(5)
+        time.sleep(1)
--- main.py
+++ main.py
@@ -1,6 +1,10 @@
 import bot
+import threading
 
 def main():
+    folder_thread = threading.Thread(target=bot.monitor_folder)
+    folder_thread.daemon = True
+    folder_thread.start()
     bot.bot.infinity_polling()
 
 if __name__ == "__main__":
